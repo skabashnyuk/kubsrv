2b07f3224ee4dca9c114a22bf2047b4cbb70fae1
Sergii Kabashniuk <skabashniuk@redhat.com>
2018-04-01 14:44:36 +0300

    Initial model

--- model/model.go
+++ model/model.go
@@ -1,1 +1,24 @@
-package main
+package model
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/api/core/v1"
+)
+
+type CheService struct {
+	metav1.TypeMeta `json:",inline"`
+	// Standard object's metadata.
+	// More info: https://git.k8s.io/community/contributors/devel/api-conventions.md#metadata
+	// +optional
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	// +optional
+	Spec CheServiceSpec `json:"spec,omitempty"`
+
+}
+
+
+type CheServiceSpec struct {
+	Services []v1.Service `json:"services,omitempty"`
+
+	Pods []v1.Pod `json:"pods,omitempty"`
+}
