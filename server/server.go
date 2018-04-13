1d9b287b59f228f93d28818aba715c5018951d09
Sergii Kabashniuk <skabashniuk@redhat.com>
2018-04-13 15:47:05 +0300

    Cleanup

--- server/server.go
+++ server/server.go
@@ -4,22 +4,12 @@ package server
 import (
 
 	"github.com/skabashnyuk/kubsrv/router"
-	"github.com/skabashnyuk/kubsrv/storage"
 	"github.com/gin-gonic/gin"
-	"github.com/nanobox-io/golang-scribble"
-	"fmt"
 )
 
 func Setup() *gin.Engine {
 	r := gin.Default()
-	dir := "./"
 
-	db, err := scribble.New(dir, nil)
-	if err != nil {
-		fmt.Println("Error", err)
-	}
-
-	r.Use(storage.SetDBtoContext(db))
 	router.Initialize(r)
 	return r
 }
