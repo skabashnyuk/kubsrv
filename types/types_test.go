942465ec5c3a029d158ecea42f6ceb90c3a93764
Sergii Kabashniuk <skabashniuk@redhat.com>
2018-04-27 15:19:42 +0300

    Test updated to new object structure

--- types/types_test.go
+++ types/types_test.go
@@ -9,142 +9,56 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestYaml(t *testing.T) {
-	dat, err := ioutil.ReadFile("test-cheservice.yaml")
-	check(err)
-	var cheService CheService
-	err = yaml.Unmarshal(dat, &cheService)
-	check(err)
-}
-
 func TestCheServiceYaml(t *testing.T) {
 	dat, err := ioutil.ReadFile("test-cheservice.yaml")
 	check(err)
+
 	expected := CheService{
-		TypeMeta: TypeMeta{
-			APIVersion: "che.eclipse.org/v1",
-			Kind:       "CheService"},
+		TypeMeta: TypeMeta{APIVersion: "v1", Kind: "CheService"},
 		ObjectMeta: ObjectMeta{
-			Name:   "io.typefox.theia-ide.che-service",
-			Labels: map[string]string(nil)},
+			Name: "io.typefox.theia-ide.che-service",
+		},
 		Spec: CheServiceSpec{
-			Services: []Service{
+			Version: Version{Version: "1.2.0"},
+			Containers: []Container{
 				{
-					TypeMeta: TypeMeta{
-						APIVersion: "v1",
-						Kind:       "Service"},
-					ObjectMeta: ObjectMeta{
-						Name:   "io.typefox.theia-ide.che-service.mainservice",
-						Labels: map[string]string(nil)},
-					Spec: ServiceSpec{
-						Ports: []ServicePort{
-							{
-								Name:       "",
-								Protocol:   "TCP",
-								Port:       80,
-								TargetPort: 9376,
-							},
-						},
-						Selector: map[string]string{"app": "theia"},
+					Image: "eclipse/che-theia:nightly",
+					Env: []EnvVar{
+						{Name: "THEIA_PLUGINS", Value: "${THEIA_PLUGINS}"},
 					},
-				},
-			},
-			Pods: []Pod{
-				{
-					TypeMeta: TypeMeta{
-						APIVersion: "v1",
-						Kind:       "Pod"},
-					ObjectMeta: ObjectMeta{
-						Name:   "io.typefox.theia-ide.che-service.mainpod",
-						Labels: map[string]string(nil),
+					Resources: ResourceRequirements{
+						Requests: ResourceList{"memory": "200Mi"},
 					},
-					Spec: PodSpec{
-						Volumes: []Volume(nil),
-						Containers: []Container{
-							{
-								Name:  "theia",
-								Image: "eclipse/che-theia:nightly",
-								Env: []EnvVar{
-									{
-										Name:  "THEIA_PLUGINS",
-										Value: "${THEIA_PLUGINS}",
-									},
-								},
-								VolumeMounts: []VolumeMount{
-									{
-										Name:      "projects",
-										ReadOnly:  false,
-										MountPath: "/projects",
-										SubPath:   "",
-									},
-								},
-							},
-							{
-								Name:         "theia-sidecar",
-								Image:        "maven:latest",
-								Env:          []EnvVar(nil),
-								VolumeMounts: []VolumeMount(nil),
-							},
+					Commands: []Command{
+						{
+							Name:       "build",
+							WorkingDir: "$(project)",
+							Command:    []string{"mvn", "clean", "install"},
 						},
 					},
-				},
-			},
-			Commands: []CheCommand{
-				{
-					TypeMeta: TypeMeta{
-						APIVersion: "che.eclipse.org/v1",
-						Kind:       "CheCommand",
-					},
-					ObjectMeta: ObjectMeta{
-						Name:   "mvn-clean",
-						Labels: map[string]string(nil),
-					},
-					Spec: CheCommandSpec{
-						TargetLabelSelector: "che.eclipse.org/container-name= theia-maven-sidecar",
-						WorkingDirectory:    "$(project)",
-						Commands: []string{"mvn", "clean", "install",
+					Servers: []Server{
+						{
+							Name:       "theia",
+							Port:       3000,
+							Protocol:   "http",
+							Attributes: map[string]string{"internal": "true", "type": "ide"},
 						},
 					},
+					Volumes: []Volume{
+						{Name: "projects", MountPath: "/projects"},
+					},
 				},
-			},
-			Version: Version{
-				Version: "1.2.2",
 			},
 		},
 	}
+
 	var object CheService
 	err = yaml.Unmarshal(dat, &object)
 	check(err)
-	print(&object)
 	// now we know that object isn't nil, we are safe to make
 	// further assertions without causing any errors
 	assert.Equal(t, expected, object, "Checking CheServiceParsing")
-}
 
-func TestServiceArrayYaml(t *testing.T) {
-	dat, err := ioutil.ReadFile("test-arrayofservice.yaml")
-	check(err)
-	var object TestStruct
-	err = yaml.Unmarshal(dat, &object)
-	check(err)
-}
-
-func TestServiceArrayJson(t *testing.T) {
-	dat, err := ioutil.ReadFile("test-arrayofservices.json")
-	check(err)
-	var object TestStruct
-	err = json.Unmarshal(dat, &object)
-	check(err)
-	print(&object)
-}
-
-func TestJson(t *testing.T) {
-	dat, err := ioutil.ReadFile("test-cheservice.json")
-	check(err)
-	var object CheService
-	err = json.Unmarshal(dat, &object)
-	check(err)
-	print(&object)
 }
 
 func check(e error) {
@@ -161,6 +75,4 @@ func print(data interface{}) {
 	}
 }
 
-type TestStruct struct {
-	Services []Service `json:"services,omitempty"`
-}
+//
