3ba4bef85ee86c44e261ed30738e156abd503a34
Sergii Kabashniuk <skabashniuk@redhat.com>
2018-05-02 14:53:13 +0300

    Added gracefull shutdown

--- main.go
+++ main.go
@@ -8,6 +8,10 @@ import (
 	"time"
 	"github.com/skabashnyuk/kubsrv/storage"
 	"flag"
+	"net/http"
+	"log"
+	"os/signal"
+	"context"
 )
 
 func main() {
@@ -52,6 +56,33 @@ func main() {
 		}
 	}
 
-	router.Run(":" + port)
+
+	srv := &http.Server{
+		Addr:    ":"+port,
+		Handler: router,
+	}
+
+	go func() {
+		// service connections
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	log.Println("Server exiting")
+
+
 
 }
